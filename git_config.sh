7369ea3d404800b61ef701fdecfc3f57ac8a3f74
Pep1sta <pcechak07@gmail.com>
2024-09-24 07:45:37 +0200

    adding the pudding

--- git_config.sh
+++ git_config.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+composer create-project laravel/laravel baka
+git config --list
 git config --global user.name "Pep1sta"
 git config --global user.email "pcechak07@gmail.com"
 echo "Přihlášení uživatele proběhlo v pořádku"
+ls
+dir
+
 #cd eshop_ferantaranyl
 # php artisan serve
